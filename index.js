7cfcc0b671a62f53f28775184ae8e51123da1702
Abhishek Alekar <abhishekalekar@gmail.com>
2020-09-14 23:47:33 +1200

    Demo search engine

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("searchInput").addEventListener("keyup", function(event) {
+    let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName('name');
+    
+
+    for (let counter = 0; counter < allNamesDOMCollection.length; counter ++)     {
+        const currentName = allNamesDOMCollection[counter].textContent.toLocaleLowerCase;
+        console.log(currentName);
+    }
+
+
+});
